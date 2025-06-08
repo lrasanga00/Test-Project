e7533bb611991033295fb80437172433b1e8d093
lrasanga00 <nhlrasanga123@gmail.com>
2025-06-08 11:03:11 +0530

    add a tail script file

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("hello Lahiru !!");
+
+// let name = "Lahiru";
+console.log(name);
+
+name=20;
+console.log(name);
+console.log(10 + name);
+
+// console.log(name == 20);
+// console.log(name == "20");
+// console.log(name === "20");
